979a311dbc3d26406f22539e1450fd09846ac003
NancyLettuce <nli4@stuy.edu>
2016-01-15 00:08:27 -0500

    Update AdmissionsOfficer.java

--- Monsters/AdmissionsOfficer.java
+++ Monsters/AdmissionsOfficer.java
@@ -1,6 +1,11 @@
+
 public class AdmissionsOfficer extends Monster {
+
     public String about() {
-        return "";
+        String story = "";
+        story += "Admissions Officers appear in the vicinity of Rubik's Cube's prison.\n";
+        story += "They serve to prevent entry by any means necessary.\n";
+        return story;
     }
     
 }
