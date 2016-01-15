7539a2127bd44ba095cdd9e97cdad29a8c337f39
NancyLettuce <nli4@stuy.edu>
2016-01-14 23:51:10 -0500

    Update Teacher.java

--- Monsters/Teacher.java
+++ Monsters/Teacher.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Teacher extends Monster {
-    public String about() {
-        return "";
-    }    
     
+    public String about() {
+        String story = "";
+        story += "Teachers appear only in Quest 1.\n";
+        story += "They will ask you questions to impede your progress.\n";
+        story += "If you subdue this monster by giving the right answer, \n"
+        story += "they will leave you with a clue to aid you in assembling your team.\n";
+        return story;
+    }    
 }
