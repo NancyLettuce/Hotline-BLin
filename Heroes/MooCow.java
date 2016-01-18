57b7f1ecb32f8af29e49a6846e3e404a6e1d32ff
NancyLettuce <nli4@stuy.edu>
2016-01-18 01:17:17 -0500

    added attack convert to Math peoples

--- Heroes/MooCow.java
+++ Heroes/MooCow.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 public class MooCow extends Character implements CSKid {
+    private final static String DIGITS = "0123456789ABCDEF";
+    
     public MooCow() {
 	super();
 	HP = 50;
@@ -15,7 +17,18 @@ public class MooCow extends Character implements CSKid {
 	String story = "Your name is MooCow\n";
 	story += "You were battling against senioritis until BLin came along.\n";
 	story += "He enlightened you on whom the real monsters you should be battling are.\n";
-	story += "Now you are ready to help him and the rest of the squad fight evil and spread love.\n";
+	story += "Now, you are ready to help him and the rest of the squad fight evil and spread love.\n";
 	return story;
+    }
+        //convert binary to another base(2 to 16)
+    public static String convert(int num, int base){
+	if (base < 2 || base > 16) {
+	    return "Please enter a base between 2 and 16";
+      	}
+      	if (num == 0) return "";
+      	return convert(num/base, base) +
+	    DIGITS.substring(num%base, (num%base)+1);
+      	//n divided by base goes into each recursion until it reaches 0
+      	//adds on the hexdigit that corresponds with the remainder of n%bas
     }
 }
--- Heroes/MrBrown.java
+++ Heroes/MrBrown.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 public class MrBrown extends Character implements CSKid {
+    private final static String DIGITS = "0123456789ABCDEF";
+    
     public MrBrown() {
 	super();
 	HP = 100;
@@ -16,4 +18,17 @@ public class MrBrown extends Character implements CSKid {
 	story += "Your name is Mr.Brown.\n";
 	return story;
     }
+
+    //convert binary to another base(2 to 16)
+    public static String convert(int num, int base){
+	if (base < 2 || base > 16) {
+	    return "Please enter a base between 2 and 16";
+      	}
+      	if (num == 0) return "";
+      	return convert(num/base, base) +
+	    DIGITS.substring(num%base, (num%base)+1);
+      	//n divided by base goes into each recursion until it reaches 0
+      	//adds on the hexdigit that corresponds with the remainder of n%bas
+    }
+
 }
