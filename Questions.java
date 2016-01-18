3fb3679f104a3f75d2aee0b9a26cd8f860989e3e
jongyoullee1 <jlee43@stuy.edu>
2016-01-18 01:15:23 -0500

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,1 +1,18 @@
 //temporary file of Questions that will be asked
+
+//Math
+public class Questions {
+    //Math
+    public static void generatePF() {
+        int a = (int) (Math.random()*5+1);
+        System.out.println("x^2+"+2*a+"x"+"+"+a*a);
+    }
+    
+    
+    //Art
+    
+    
+    public static void main(String[] args) {
+        generatePF();
+    }
+}
