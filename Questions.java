e0e3eeb8dd68bfeb719979503ce28f7b8246eda5
jongyoullee1 <jlee43@stuy.edu>
2016-01-18 19:47:09 -0500

    Update Questions.java
    Made a type of problem the player will be asked.

--- Questions.java
+++ Questions.java
@@ -1,11 +1,21 @@
-//temporary file of Questions that will be asked
+import java.io.*;
+import java.util.*;
 
-//Math
 public class Questions {
     //Math
-    public static void generatePF() {
+    public static boolean generatePF() {
         int a = (int) (Math.random()*5+1);
         System.out.println("x^2+"+2*a+"x"+"+"+a*a);
+        try {
+            System.out.println("Find a root of the quadratic:");
+            Scanner sc = new Scanner(System.in);
+            int ans = sc.nextInt(); // Learn to use this on the docs. Also, please use tabs. :'( The whitespace made me sad.
+            if (ans==a) return true;
+        }
+        catch (Exception e) { 
+        return false;
+        }
+        return false;
     }
     
     
@@ -13,6 +23,6 @@ public class Questions {
     
     
     public static void main(String[] args) {
-        generatePF();
+        System.out.println(generatePF());
     }
 }
