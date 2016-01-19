eb6205b63e6a8897e7c782f0943023a9cb62af15
NancyLettuce <nli4@stuy.edu>
2016-01-18 23:58:14 -0500

    Added decimal

--- Heroes/MooCow.java
+++ Heroes/MooCow.java
@@ -23,7 +23,8 @@ public class MooCow extends Character implements CSKid {
 	story += "Now, you are ready to help him and the rest of the squad fight evil and spread love.\n";
 	return story;
     }
-        //convert binary to another base(2 to 16)
+    
+    //convert binary to another base(2 to 16)
     public static String convert(int num, int base){
 	if (base < 2 || base > 16) {
 	    return "Please enter a base between 2 and 16";
@@ -33,5 +34,21 @@ public class MooCow extends Character implements CSKid {
 	    DIGITS.substring(num%base, (num%base)+1);
       	//n divided by base goes into each recursion until it reaches 0
       	//adds on the hexdigit that corresponds with the remainder of n%bas
+    }
+
+    //convert to Decimal from any base
+    public int decimal(String s, int base) {
+	int dec = 0;
+	int power = 0;
+	for (int ctr = 0; ctr < s.length(); ctr ++ ) {
+	    power = s.length() - ctr - 1;
+	    //ctr and power increment at opposite ends
+	    dec += (DIGITS.indexOf(s.substring(ctr,ctr+1))
+		    //goes through each char in string s
+		    //returns corresponding index of char in string HEXDIGITS
+		    * Math.pow(base, power));
+	    //number multiplied by base raised to th correct power
+	}
+	return dec;
     }
 }
--- Heroes/MrBrown.java
+++ Heroes/MrBrown.java
@@ -34,4 +34,20 @@ public class MrBrown extends Character implements CSKid {
       	//adds on the hexdigit that corresponds with the remainder of n%bas
     }
 
+    //convert to Decimal from any base
+    public int decimal(String s, int base) {
+	int dec = 0;
+	int power = 0;
+	for (int ctr = 0; ctr < s.length(); ctr ++ ) {
+	    power = s.length() - ctr - 1;
+	    //ctr and power increment at opposite ends
+	    dec += (DIGITS.indexOf(s.substring(ctr,ctr+1))
+		    //goes through each char in string s
+		    //returns corresponding index of char in string HEXDIGITS
+		    * Math.pow(base, power));
+	    //number multiplied by base raised to th correct power
+	}
+	return dec;
+    }
+
 }
