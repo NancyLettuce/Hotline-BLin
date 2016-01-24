e9aa2a64205dcf88be12ab88b8155b2008822c60
NancyLettuce <nli4@stuy.edu>
2016-01-24 06:39:25 -0500

    main fixed

--- Monster.java
+++ Monster.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public abstract class Monster{
     //Basic properties of all characters.
@@ -11,6 +12,47 @@ public abstract class Monster{
     int defInitial = 0;
     int hpInitial = 0;
     //end of properties
+
+    //for asking questions==================================================
+    public ArrayList<String> types;//no real use, just to track methods
+    private Map<String, String> map = new HashMap<String, String>();
+    private ArrayList<String> names;
+    private ArrayList<String> works;
+    int random = -1; //randomly generated number 
+    int randMethod = -1; //number to keep track of methods
+    
+    //stores answer choices
+    private String choice;
+
+    protected int a = (int) (Math.random()*5+1);
+    //correct answer for the math find root question
+    protected int b = (int) (Math.random()*38+2);
+    //correct answer for the math isPrime question
+    private String title; //should store the thing you are looking for
+    //for artists, stores painting names
+    
+    protected int correctArt; //stores correct answer;
+    //======================================================
+    
+    //constructor
+    public Monster() {
+        types = new ArrayList<String>();
+        random = (int)(Math.random() * (types.size()+1));
+        types.add("askRoot");
+        types.add("askArtist");
+        types.add("populate");
+        types.add("askPrime");
+        map.put("\"The Water Lily Pond\"", "Claude Monet");
+        map.put("Ballet Rehearsal", "Edgar Degas");
+        map.put("\"The Basket of Apples\"", "Paul Cezanne");
+        map.put("\"By the Seashore\"", "Pierre Renoir");
+        map.put("\"The Potato Eaters\"", "Vincent van Gogh");
+        map.put("\"The Persistence of Memory\"", "Salvador Dali");
+        map.put("Canvas Covered in Paint Drops", "Jackson Pollock");
+        map.put("\"Portrait of Adele Bloch-Bauer\"", "Gustave Klimt");
+        map.put("The son of Man", "Rene Magritte");
+        map.put("Fruits", "Paul Cezanne");        
+    }
     
     //Dead or alive?
     public boolean isAlive(){
@@ -34,6 +76,156 @@ public abstract class Monster{
         int damage = (int)(Math.random() * (this.str - other.def));
         other.HP -= damage;
         return damage;
+    }
+
+
+    //asking questions ================================================
+    
+    //arrayList of artist names
+    public void listOfNames() {
+        names = new ArrayList<String>(map.values());
+        //ArrayList names populated with keySet();
+    }
+    
+    public void listOfWorks() {
+        works = new ArrayList<String>(map.keySet());
+        //ArrayList works populated with values of dictionary map
+    }
+    
+    public void askPrime() {
+        b = (int) (Math.random()*38+2);
+        System.out.println("Is "+b+" prime?");
+        System.out.println("\t1. True");
+        System.out.println("\t2. False");
+    }
+    
+    public boolean isPrime(int p) {
+	if (p == 1) {
+	    return false;
+	}
+	if (p == 2) {
+	    return true;
+	}
+	for (int i = 2; i <= Math.sqrt(p) + 1; i++) {
+	    if (p % i == 0) {
+		return false;
+	    }
+	}
+	return true;
+    } 
+    
+    public void askRoot() {
+        a = (int) (Math.random()*5+1);
+        System.out.println("Find a root of the quadratic:");
+        System.out.println("\tx^2+"+2*a+"x"+"+"+a*a);
+    }
+    
+    //CS array generator
+    public ArrayList populate( int size, int lo, int hi ) {
+    	ArrayList<Integer> retAL = new ArrayList<Integer>();
+    	while( size > 0 ) {
+    	    //     offset + rand int on interval [lo,hi]
+    	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+    	    size--;
+    	}
+    	return retAL;
+    }
+    //Art
+    
+    
+    public void askArtist() {
+        ArrayList<String> nameCopy = names;//copy of names
+        ArrayList<String> titleCopy = works;//copy of works
+        ArrayList<String> answerChoices = new ArrayList<String>(4);//stores asnwer choices
+        //stores values for each choice
+        String A = "";
+        String B = "";
+        String C = "";
+        String D = "";
+        
+        random = (int)(Math.random() * titleCopy.size());//generates random int
+        title = titleCopy.get(random);//used to determine random work to ask about
+        titleCopy.remove(title);//deleted to avoid repetition
+        
+        String multiChoice1[] = {"Who is the artist who painted " + title + "?"};
+        int scorePro;
+        int score;
+        Scanner input = new Scanner(System.in);
+        String userSelect1 =  multiChoice1[0];
+        
+        answerChoices.add(map.get(title));//adds artist who painted it to answer choices
+        nameCopy.remove(map.get(title));//removes it from list of names
+        for (int ctr = 0; ctr < 3; ctr ++) {
+            String randName = "";
+            random = (int)(Math.random() * (nameCopy.size()-1));
+            randName = nameCopy.get(random);
+            answerChoices.add(randName);//adds random name to answer choice
+            nameCopy.remove(randName);//removes that name from nameCopy
+        }
+
+        if (userSelect1.equals(multiChoice1[0])) {
+            System.out.println(multiChoice1[0]);
+            random = (int)(Math.random() * (answerChoices.size()-1));
+            //randomizes which element from answerChoice is used here
+            A = answerChoices.get(random);//artist name
+            System.out.println("\t1." + A);
+            answerChoices.remove(answerChoices.get(random));//removes to avoid repetition
+            //basically the same code repeated
+            random = (int)(Math.random() * (answerChoices.size()-1));
+            B = answerChoices.get(random);
+            System.out.println("\t2." + B);
+            answerChoices.remove(answerChoices.get(random));
+            
+            random = (int)(Math.random() * (answerChoices.size()-1));
+            C = answerChoices.get(random);
+            System.out.println("\t3." + C);
+            answerChoices.remove(answerChoices.get(random));
+            
+            random = (int)(Math.random() * (answerChoices.size()-1));
+            D = answerChoices.get(random);
+            System.out.println("\t4." + D);
+            
+            System.out.println();
+            
+        }
+        
+        if (map.get(title) == A) {
+            //if artist corresponding to the painting is A...
+            correctArt = 1;
+            //the correct choice is A
+        }
+        else if (map.get(title) == B) {
+            correctArt = 2;
+        }
+        else if (map.get(title) == C) {
+            correctArt = 3;
+        }
+        else if (map.get(title) == D) {
+            correctArt = 4;
+        }
+            
+    }
+    
+    // more multiple choice questions with similar structure
+    public void askQuestion() {
+        randMethod = (int)(Math.random() * types.size());
+        listOfNames();
+        listOfWorks();
+        System.out.println(types.get(randMethod));
+        if (types.get(randMethod) == "askArtist") {
+            askArtist();
+        }
+        else if (types.get(randMethod) == "askRoot") {
+            askRoot();
+        }
+        else if (types.get(randMethod) == "askPrime") {
+            askPrime();
+        }
+        /*
+	  else if (types.get(random) == "populate") {
+	  populate();
+	  }
+        */
     }
 }
 
