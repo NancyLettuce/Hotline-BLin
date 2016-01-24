1f67bc9074beabe8b84652968f10160141135478
NancyLettuce <nli4@stuy.edu>
2016-01-24 13:55:42 -0500

    regarding clues and arraylists

--- Monster.java
+++ Monster.java
@@ -36,7 +36,21 @@ public abstract class Monster{
     //for artists, stores painting names
     protected String sorted;//correct answer for sorted array
     protected int correctArt; //stores correct answer;
-    //======================================================
+
+    //====for clues==================================================
+    protected ArrayList<String> alice;
+    protected ArrayList<String> drake;
+    protected ArrayList<String> ernie;
+    protected ArrayList<String> mooCow;
+    protected ArrayList<String> mrBrown;
+    
+    //=====final answers==========================
+    protected String aliceAns;
+    protected String drakeAns;
+    protected String ernieAns;
+    protected String mooAns;
+    protected String brownAns;
+    //=======================================================
     
     //constructor
     public Monster() {
@@ -55,7 +69,54 @@ public abstract class Monster{
         map.put("Canvas Covered in Paint Drops", "Jackson Pollock");
         map.put("\"Portrait of Adele Bloch-Bauer\"", "Gustave Klimt");
         map.put("The son of Man", "Rene Magritte");
-        map.put("Fruits", "Paul Cezanne");        
+        map.put("Fruits", "Paul Cezanne");
+	        map.put("Knight, Devil, and Death", "Albrecht Durer");//10
+        map.put("\"Girl with a Pearl Earring\"", "Johannes Vermeer");
+        map.put("chubby naked ladies", "Peter Paul Rubens");
+        map.put("\"The Third of May\"", "Francisco Goya");
+        map.put("\"Arnolfini Portrait\"", "Jan Van Eyck");
+        map.put("\"Several Circles\"", "Wassily Kandinsky");//15
+        map.put("pop art", "Andy Warhol");
+        map.put("Garden of Earthly Delights", "Hiernymous Bosch");
+        map.put("\"View of Toledo\"", "El Greco");
+        map.put("\"American Gothic\"", "Grant Wood");
+        map.put("\"I and the Village\"", "Marc Chagall");//20
+        map.put("\"The Death of Marat\"", "Jacques-Louis David");
+        map.put("houses and train stations", "Edward Hopper");
+        map.put("the American flag", "Jasper Johns");
+        map.put("\"Self Portrait with a Monkey\"", "Frida Kahlo");
+        map.put("murals", "Diego Rivera");//25
+        map.put("\"Guernica\"", "Pablo Picasso");
+        map.put("\"The Night Watch\"", "Rembrandt Van Rijn");
+        map.put("\"Las Meninas\"", "Diego Velazquez");
+        map.put("The Calling of St. Matthew", "Caravaggio");
+        map.put("The Yellow Christ", "Paul Gauguin");//30
+        map.put("\"Olympia\"", "Edouard Manet");
+        map.put("Dance", "Henri Matisse");
+        map.put("\"School of Athens\"", "Raphael");
+        map.put("\"Lady Liberty Leading the People\"", "Eugene Delacroix");
+        map.put("readymades", "Marcel Duchamp");//35
+        map.put("\"Sunday Afternoon on the Island of La Grand Jatte\"", "Georges Seurat");
+        map.put("\"Birth of Venus\"", "Sandro Botticelli");
+        map.put("\"Drowning Girl\"", "Roy Lichtenstein");
+        map.put("close ups of flowers", "Georgia O'Keefe");
+        map.put("ships and storms", "J M W Turner");//40
+        map.put("\"The Ambassadors\"", "Hans Holbein the Younger");
+        map.put("colorful squares", "Piet Mondrian");
+        map.put("\"Black Square\"", "Kazimir Malevich");
+        map.put("\"Portrait of Madame X\"", "John Singer Sargeant");
+        map.put("\"Ad Parnassum\"", "Paul Klee");//45
+        map.put("\"The Four Seasons\"", "Nicolas Poussin");
+        map.put("\"Tennis at Newport\"", "George Bellows");
+        map.put("portraits of people in pain", "Francis Bacon");
+        map.put("\"Red Headed Girl in Evening\"", "Modigliani");
+        map.put("\"The Hunters in the Snow\"", "Pieter Bruegel the Elder");//50
+
+	alice = new ArrayList<String>(5);    
+        drake = new ArrayList<String>(5);    
+        ernie = new ArrayList<String>(5);        
+        mooCow = new ArrayList<String>(5);        
+        mrBrown = new ArrayList<String>(5);
     }
     
     //Dead or alive?
--- Teacher.java
+++ Teacher.java
@@ -10,6 +10,30 @@ public class Teacher extends Monster {
         HP = 300; 
         def = 20;
         str = 100;
+	
+	alice.add("The depths of hell");
+        alice.add("The source of all my torment");
+        alice.add("A raging inferno");
+        alice.add("RIP my grades");
+        alice.add("Where hopes and dreams go to die");
+        
+        drake.add("He is a famous singer");
+        drake.add("He was featured on a Rihanna song");
+        drake.add("Pure, lyrical genius");
+        drake.add("Revolutionary math");
+        drake.add("Thinking outside of the box");
+        
+        ernie.add("The only constant in life is change");
+        ernie.add("Second term senior");
+        ernie.add("The aftermath of college acceptance");
+        ernie.add("Past tense");
+        ernie.add("To become different");
+        
+        aliceAns = "stuyvesant";
+        drakeAns = "8 something";
+        ernieAns = "you've changed";
+        mooAns = "idk yet";
+        brownAns = "please pass us";
     }
     //you can choose to answer question or to attack normally
     //if you answer one question correctly, you defeat a teacher
