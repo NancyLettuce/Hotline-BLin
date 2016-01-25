e8be56dfd18cd75ccf43089a638e06867e4e1acb
jongyoullee1 <jlee43@stuy.edu>
2016-01-24 22:37:31 -0500

    Update Alice.java

--- Alice.java
+++ Alice.java
@@ -19,7 +19,7 @@ public class Alice extends Character implements  ArtKid {
 	
     public String about() {
         String alice = "";
-	alice += "Your name is Alice.\n";
+	alice += "\nYour name is Alice.\n";
 	alice += "Although you have a tendency to eat whatever you see, you are highly skilled in art.\n";
 	alice += "BLin recruited you after your heartrending drawing of a Rubik's Cube in the perfect lighting.\n";
 	alice += "Now you are ready to help him and the rest of the squad find your painting model.\n";
