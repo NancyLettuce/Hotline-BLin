7027e150225263ca08ded358eceeca1e3d688a43
NancyLettuce <nli4@stuy.edu>
2016-01-25 02:22:53 -0500

    update Blin

--- BLinArt.java
+++ BLinArt.java
@@ -19,20 +19,8 @@ public class BLinArt extends Character implements ArtKid {
     }
 	
     public String about() {
-	String story = "Your name is Hotline BLin.\n";
-	story += "Starting last week, your girlfriend, Rubik's Cube, has been acting quite distant.\n";
-	story += "She did not return your texts, calls, or emails. Then you finally realized: She's missing.\n";
-	story += "Here is a picture of the love of your life:\n";
-	story += "    ___ ___ ___\n" +
-	    "   /___/___/___/|\n" +
-	    "  /___/___/___/||\n" +
-	    " /___/___/__ /|/|\n" +
-	    "|   |   |   | /||\n" +
-	    "|___|___|___|/|/|\n" +
-	    "|   |   |   | /||\n" +
-	    "|___|___|___|/|/\n" +
-	    "|   |   |   | /\n" +
-	    "|___|___|___|/\n";
+        String story = "Despite the fact that you draw cooked stick figures, you love art.\n";
+	story += "Your knowledge of art may come in handy to your teammates.\n";
 	return story;
     }
 
--- BLinCS.java
+++ BLinCS.java
@@ -20,20 +20,8 @@ public class BLinCS extends Character implements CSKid {
     }
 	
     public String about() {
-	String story = "Your name is Hotline BLin.\n";
-	story += "Starting last week, your girlfriend, Rubik's Cube, has been acting quite distant.\n";
-	story += "She did not return your texts, calls, or emails. Then you finally realized: She's missing.\n";
-	story += "Here is a picture of the love of your life:\n";
-	story += "    ___ ___ ___\n" +
-	    "   /___/___/___/|\n" +
-	    "  /___/___/___/||\n" +
-	    " /___/___/__ /|/|\n" +
-	    "|   |   |   | /||\n" +
-	    "|___|___|___|/|/|\n" +
-	    "|   |   |   | /||\n" +
-	    "|___|___|___|/|/\n" +
-	    "|   |   |   | /\n" +
-	    "|___|___|___|/\n";
+        String story = "You win all of the local coding competitions.\n";
+	story += "Although random juniors message you at 3 in the morning about CS questions, life is good.\n";
 	return story;
     }
 
