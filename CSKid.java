15ddb995166e1f0648caf01a6e42819371ff9f68
NancyLettuce <nli4@stuy.edu>
2016-01-25 19:51:46 -0500

    here

---
+++ CSKid.java
@@ -0,0 +1,10 @@
+
+public interface CSKid {
+    //public void convert(int a);
+    //public void sort(ArrayList a);
+    
+    //public void binary(int a);
+    //public void convert(int num, int base);
+}
+
+   
